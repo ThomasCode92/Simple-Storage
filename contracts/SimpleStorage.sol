ef8f491e86c94fff85d648b6269dbf97ab265f35
ThomasCode92 <thomascode92@hotmail.com>
2023-08-16 08:10:08 +0200

    Inheritance & Overrides

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -12,7 +12,7 @@ contract SimpleStorage {
         string name;
     }
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
 
