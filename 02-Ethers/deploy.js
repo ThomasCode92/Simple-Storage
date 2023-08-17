bf0430b83197de38e1c84027de9cf5856a0555fa
ThomasCode92 <thomascode92@hotmail.com>
2023-08-17 18:29:20 +0200

    Optional Prettier Formatting

--- 02-Ethers/deploy.js
+++ 02-Ethers/deploy.js
@@ -1,33 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const ethers = require("ethers");
-const dotenv = require("dotenv");
+const ethers = require('ethers');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 async function main() {
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  const encryptedJson = fs.readFileSync('./.encryptedKey.json', 'utf8');
   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 
   let wallet = ethers.Wallet.fromEncryptedJsonSync(
     encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
+    process.env.PRIVATE_KEY_PASSWORD,
   );
 
   wallet = wallet.connect(provider);
 
   const abi = fs.readFileSync(
-    "./dist/contracts_SimpleStorage_sol_SimpleStorage.abi",
-    "utf8"
+    './dist/contracts_SimpleStorage_sol_SimpleStorage.abi',
+    'utf8',
   );
 
   const binary = fs.readFileSync(
-    "./dist/contracts_SimpleStorage_sol_SimpleStorage.bin",
-    "utf8"
+    './dist/contracts_SimpleStorage_sol_SimpleStorage.bin',
+    'utf8',
   );
 
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
+  console.log('Deploying, please wait...');
 
   const contract = await contractFactory.deploy({ gasLimit: 3000000 });
   console.log(contract);
@@ -38,7 +38,7 @@ async function main() {
   const currentFavoriteNumber = await contract.retrieve();
   console.log(`Current Favorite Number: ${currentFavoriteNumber}`);
 
-  const transactionResponse = await contract.store("7");
+  const transactionResponse = await contract.store('7');
   const transactionReceipt = await transactionResponse.wait();
   const updatedFavoriteNumber = await contract.retrieve();
   console.log(`Updated Favorite Number: ${updatedFavoriteNumber}`);
@@ -46,7 +46,7 @@ async function main() {
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.log(error);
     process.exit(1);
   });
