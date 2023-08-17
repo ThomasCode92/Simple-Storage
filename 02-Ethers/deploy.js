4998a92286ff8de6fb328f1c596fcf64efaa8d17
ThomasCode92 <thomascode92@hotmail.com>
2023-08-17 13:36:42 +0200

    Introduction to Ethers.js

---
+++ 02-Ethers/deploy.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const ethers = require("ethers");
+
+async function main() {
+  const provider = new ethers.JsonRpcProvider("http://172.22.112.1:7545");
+  const wallet = new ethers.Wallet(
+    "0x82a1c161b5ac80fc8facc81b0aee286ec838f5408f0805c5e4d12b1935f4019d",
+    provider
+  );
+
+  const abi = fs.readFileSync(
+    "./dist/contracts_SimpleStorage_sol_SimpleStorage.abi",
+    "utf8"
+  );
+
+  const binary = fs.readFileSync(
+    "./dist/contracts_SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+
+  const contract = await contractFactory.deploy({ gasLimit: 3000000 });
+  console.log(contract);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
