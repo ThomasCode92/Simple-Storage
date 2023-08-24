02d0783af51be38eeac39718e237a534c0cb8deb
ThomasCode92 <thomascode92@hotmail.com>
2023-08-24 12:42:49 +0200

    Programmatic Verification

--- 03-Hardhat/scripts/deploy.js
+++ 03-Hardhat/scripts/deploy.js
@@ -1,4 +1,7 @@
-const { ethers } = require('hardhat');
+const { ethers, run, network } = require('hardhat');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
@@ -6,6 +9,26 @@ async function main() {
 
   await simpleStorage.waitForDeployment();
   console.log(`Deployed contract to: ${simpleStorage.target}`);
+
+  if (process.env.ETHERSCAN_API_KEY && network.config.chainId === 11155111) {
+    await simpleStorage.deploymentTransaction().wait(6);
+    await verify(simpleStorage.target);
+  }
+}
+
+async function verify(contractAddress, args) {
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes('already verified')) {
+      return console.log('Already Verified');
+    }
+
+    console.error(error);
+  }
 }
 
 main()
