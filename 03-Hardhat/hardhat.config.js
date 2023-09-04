7ca2c4f3e09645d483431b7d57e544b0c84b21e2
ThomasCode92 <thomascode92@hotmail.com>
2023-09-04 08:13:39 +0200

    Hardhat Localhost Node

--- 03-Hardhat/hardhat.config.js
+++ 03-Hardhat/hardhat.config.js
@@ -13,6 +13,10 @@ module.exports = {
   solidity: '0.8.19',
   defaultNetwork: 'hardhat',
   networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
+    },
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
